2a87adef529944d91fb7ed1f79578dff3bcbb698
Gabriel <gabriel@Admins-MacBook-Pro.local>
2021-05-20 14:20:42 -0500

    added playlist/css

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
+// playlist addFaveSong
 
 const myPlaylist = (props) => {
+    const {playlist, addFaveSong} = props
 
-    return (
+    const loaded = () => (
         <div>
-            
+        {playlist.map((song) => (
+            <h3>{playlist.title}</h3>
+            <h3>{playlist.artist}</h3>
+            <h4>{playlist.time}</h4>
+            <button
+              onClick={() => 
+                addFaveSong(song)}>
+                <3
+            </button>
+        ))}
+        </div>
+    );
+
+    const loading = () => (
+        <div>
+            <h1>Loading...</h1>
         </div>
     )
+
+    return playlist.length > 0 ? loaded() : loading()
 }
 
 export default myPlaylist
